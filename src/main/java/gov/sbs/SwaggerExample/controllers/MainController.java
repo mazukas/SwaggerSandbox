eb9ffa3cff454ea0d4111d7043972dd8cb3555c8
MATTHEW AZUKAS <matthewazukas@MATTHEWs-MBP.home>
2015-06-27 17:11:32 -0400

    Updates for blame tool

--- src/main/java/gov/sbs/SwaggerExample/controllers/MainController.java
+++ src/main/java/gov/sbs/SwaggerExample/controllers/MainController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController(value = "main")
 public class MainController {
 
-	private String temp = "";
+	private String badVar = "";
+	private String badVar2 = "";
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello() {
